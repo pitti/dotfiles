e3f0ec38b315db2c78f523ef6c37451025f9ea73
Philipp Ittershagen <Philipp.Ittershagen@Informatik.Uni-Oldenburg.DE>
2011-07-01 13:14:24 +0200

    Add suspend and media key bindings to awesome.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -387,6 +387,13 @@ globalkeys = awful.util.table.join(
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
               end),
+
+    awful.key( { }, "XF86AudioPrev",        function () awful.util.spawn("banshee --previous", false)end),
+    awful.key( { }, "XF86AudioNext", function () awful.util.spawn("banshee --next", false) end),
+    awful.key( { }, "XF86AudioPlay", function () awful.util.spawn("banshee --toggle-playing", false) end),
+
+    awful.key( { }, "XF86PowerOff", function () awful.util.spawn("sudo pm-suspend", false) end),
+
     awful.key( { }, "XF86AudioMute",        function () awful.util.spawn("dvol -t", false)end),
     awful.key( { }, "XF86AudioRaiseVolume", function () awful.util.spawn("dvol -i 5", false) end),
     awful.key( { }, "XF86AudioLowerVolume", function () awful.util.spawn("dvol -d 5", false) end)
