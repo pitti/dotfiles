2ded373f3b787364a9f4a672c0317cacf05c8006
Philipp Ittershagen <philipp@gate-nine.de>
2011-10-18 10:48:15 +0200

    Add Rule for Pidgin to spawn on 1st tag of 2nd screen.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -478,6 +478,9 @@ awful.rules.rules = {
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
+		{ rule = { class = "Pidgin" }, properties = { tag = tags[2][1]}},
+
+    --   properties = { tag = tags[1][2] } },
 }
 -- }}}
 
