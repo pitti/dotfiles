b84dc9120040e8e9612c01dffc755c52035aee78
Philipp Ittershagen <Philipp.Ittershagen@Informatik.Uni-Oldenburg.DE>
2012-03-18 20:13:55 +0100

    Use dmenu_run for super+space.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -436,7 +436,8 @@ globalkeys = awful.util.table.join(
 
    awful.key( { }, "XF86AudioMute",        function () awful.util.spawn("dvol -t", false)end),
    awful.key( { }, "XF86AudioRaiseVolume", function () awful.util.spawn("dvol -i 5", false) end),
-   awful.key( { }, "XF86AudioLowerVolume", function () awful.util.spawn("dvol -d 5", false) end)
+   awful.key( { }, "XF86AudioLowerVolume", function () awful.util.spawn("dvol -d 5", false) end),
+   awful.key( { modkey }, "space", function () awful.util.spawn("dmenu_run", false) end)
 
 )
 
