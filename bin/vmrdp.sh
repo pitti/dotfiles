3792370d5302322dcf32abe320958ba52664e898
Philipp Ittershagen <Philipp.Ittershagen@offis.de>
2012-06-29 14:27:10 +0200

    Initial import of bin/ scripts.

---
+++ bin/vmrdp.sh
@@ -0,0 +1,17 @@
+#/!bin/sh
+
+MACHINE=`basename "$0" | cut -s -f2- -d-`
+WINUSER=philippi
+RESOLUTION=1276x1000
+
+exec xfreerdp -0 -a 24  -x l \
+           --sec rdp \
+           -g $RESOLUTION \
+           -d TRANSPORTATION -u $WINUSER \
+           --plugin rdpsnd \
+           --plugin cliprdr \
+           --plugin drdynvc \
+             --data audin:alsa tsmf:audio:alsa -- \
+           --plugin rdpdr   \
+             --data disk:home:/home/$USER printer:PDF -- \
+           $MACHINE.ddns.lcl
