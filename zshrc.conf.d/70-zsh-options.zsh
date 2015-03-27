59f47830872f01f19f5335c518f9afeb28666af3
Philipp Ittershagen <Philipp.Ittershagen@offis.de>
2015-03-27 11:05:15 +0100

    Add fix for umlauts and word boundaries.

--- zshrc.conf.d/70-zsh-options.zsh
+++ zshrc.conf.d/70-zsh-options.zsh
@@ -8,3 +8,11 @@ setopt HIST_IGNORE_SPACE
 
 # key timeout for things like VI mode
 export KEYTIMEOUT=1
+
+# german umlauts should not be counted as word boundaries for things
+# like Ctrl-W
+export WORDCHARS="äöüß"
+
+autoload -U select-word-style
+select-word-style normal
+
