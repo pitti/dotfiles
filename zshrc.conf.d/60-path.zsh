500d5a00a361ff5a50f6648ea9e90b00264d2c85
Philipp Ittershagen <Philipp.Ittershagen@offis.de>
2015-03-27 11:05:25 +0100

    Set LD_LIBRARY_PATH.

--- zshrc.conf.d/60-path.zsh
+++ zshrc.conf.d/60-path.zsh
@@ -3,3 +3,10 @@
 typeset -U path
 
 path=(~/.local/bin $path)
+
+typeset -T LD_LIBRARY_PATH ld_library_path
+typeset -U ld_library_path
+
+ld_library_path=(~/.local/lib /usr/local/lib)
+
+export LD_LIBRARY_PATH
