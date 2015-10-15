20880689322680a4fbb3dfde11cd2e66a1a230b3
Philipp Ittershagen <Philipp.Ittershagen@offis.de>
2015-10-15 17:17:24 +0200

    Add convenience completion options.

--- zshrc.conf.d/50-completion.zsh
+++ zshrc.conf.d/50-completion.zsh
@@ -2,4 +2,10 @@
 
 # Rehash on every completed command.
 zstyle ":completion:*:commands" rehash 1
+
+# complete .. to ../
 zstyle ':completion:*' special-dirs true
+
+
+# Complete for files after equal sign (FOO=/..)
+setopt magic_equal_subst
