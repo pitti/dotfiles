2e203964e38c8992fd66baf30794d1c5358a7b94
Philipp Ittershagen <pit@shgn.de>
2015-10-23 16:05:00 +0200

    Use completion settings from oh-my-zsh.

--- zshrc.conf.d/50-completion.zsh
+++ zshrc.conf.d/50-completion.zsh
@@ -1,8 +1,21 @@
+unsetopt menu_complete   # do not autoselect the first completion entry
+unsetopt flowcontrol
+setopt auto_menu         # show completion menu on succesive tab press
+setopt complete_in_word
+setopt always_to_end
+
 
 autoload -U compinit
 
 compinit
 
-# Rehash on every completed command.
-zstyle ":completion:*:commands" rehash 1
+zmodload -i zsh/complist
+
+
 zstyle ':completion:*' special-dirs true
+
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
