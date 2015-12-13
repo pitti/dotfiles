5da5d9eff97cc7fb654203a4626b3e30d122467a
Philipp Ittershagen <pit@shgn.de>
2015-12-13 22:28:52 +0100

    Add LLVM 3.7 (debian stretch) to default path.

--- zshrc.conf.d/00-path.zsh
+++ zshrc.conf.d/00-path.zsh
@@ -2,7 +2,7 @@
 # unique entries in path array (which is tied to $PATH)
 typeset -U path
 
-path=(~/.local/bin $path)
+path=(~/.local/bin /usr/lib/llvm-3.7/bin $path)
 
 typeset -T LD_LIBRARY_PATH ld_library_path
 typeset -U ld_library_path
